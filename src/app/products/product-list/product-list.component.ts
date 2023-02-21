b3a7c3ea7220b85164edbe3271dd4cc0a028c3b4
Johnny <johnnyflores@live.ca>
2023-02-21 14:14:27 -0500

    Created product service

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,19 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit{
 
   @Output()
   productWasSelected = new EventEmitter<Product>();
 
-  products: Product[] = [
-    new Product('Mobile Phone', 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.', 'https://images.unsplash.com/photo-1589492477829-5e65395b66cc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'),
-  ]
+  products!: Product[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products = this.productService.getProducts();
+  }
 
   onProductSelected(product: Product) {
     this.productWasSelected.emit(product);
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { Product } from './product.model';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
+  providers: [ProductService]
 })
 export class ProductsComponent {
 
