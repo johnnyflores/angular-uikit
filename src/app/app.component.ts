f8b656b445af574983e23c3d18e0ac8e1e15dd3b
Johnny <johnnyflores@live.ca>
2023-03-06 15:53:44 -0500

    Added autologin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { TranslateService } from "@ngx-translate/core";
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   title = 'angular-uikit';
 
-  constructor(private http: HttpClient, private translate: TranslateService) {
+  constructor(private http: HttpClient,
+              private translate: TranslateService,
+              private authService: AuthService) {
 
     translate.addLangs(['en', 'fr']);
     translate.setDefaultLang('en');
     const browserLang = translate.getBrowserLang();
     translate.use(browserLang?.match(/en|fr/) ? browserLang : 'en');
 
+  }
+
+  ngOnInit(): void {
+    this.authService.autoLogin()
   }
 }
