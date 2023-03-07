6a1d59d00a8cd5982986a819d686cac4ffa26acd
Johnny <johnnyflores@live.ca>
2023-03-07 12:09:59 -0500

    Added canActivate

--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "../auth/auth-guard";
 import { ProductDetailComponent } from "./product-detail/product-detail.component";
 import { ProductEditComponent } from "./product-edit/product-edit.component";
 import { ProductStartComponent } from "./product-start/product-start.component";
@@ -10,6 +11,7 @@ const routes: Routes = [
     {
         path: '',
         component: ProductsComponent,
+        canActivate: [AuthGuard],
         children: [
             {
                 path: '',
